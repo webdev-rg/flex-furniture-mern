94a9483ec38a797d22d9374de461a59ce0bbe709
Your Name <you@example.com>
2024-10-03 12:04:08 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const categoryModel = require("./models/categoryModel");
 const app = express();
 const PORT = 1901;
 
-app.use(cors({ origin: "https://flex-furniture-backend.onrender.com" }));
+app.use(cors());
 app.use(express.json());
 
 //? Admin API
@@ -24,7 +24,7 @@ app.post("/api/createadmin", async (req, res) => {
     if (existingAdmin) {
       return res.status(400).json({ message: "Admin already exists" });
     }
-    
+
     const data = await adminModel({
       adminEmail: email,
       adminPassword: password,

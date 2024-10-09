23ee3e8edce5614ab1d9a02dcca95bba49be786d
Your Name <you@example.com>
2024-10-09 21:21:48 +0530

    Fetched categories on user side

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -1,19 +1,77 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper/modules";
+import { Loading } from "../Loading/Loading";
 
-//? Category Images
-import chairCategory from "/images/chair-category.png";
-import lampCategory from "/images/lamp-category.png";
-import tableCategory from "/images/table-category.png";
-import bedCategory from "/images/bed-category.png";
-import sofaCategory from "/images/sofa-category.png";
+export const Category = ({ prevButton, nextButton }) => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const swiperRef = useRef(null);
 
-export const Category = ({ prevButton, nextButton, slides }) => {
+  const handleGetCategories = async () => {
+    try {
+      const response = await fetch(
+        "https://flex-furniture-server.onrender.com/api/getcategories",
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      const data = await response.json();
+
+      if (!data || data.message === "No category found") {
+        alert(`${data.message}`);
+        setLoading(false); // End loading
+        return;
+      }
+
+      const categoriesWithImages = await Promise.all(
+        data.map(async (item) => {
+          try {
+            const imageBlobRes = await fetch(
+              `https://flex-furniture-server.onrender.com/api/getcategoryimage/${item._id}`
+            );
+
+            if (!imageBlobRes.ok) {
+              throw new Error(`Error fetching image for category ${item._id}`);
+            }
+
+            const imageBlobData = await imageBlobRes.json();
+            return { ...item, imageURL: imageBlobData.imageDataUrl };
+          } catch (error) {
+            console.error(
+              `Error fetching image for category ${item._id}:`,
+              error
+            );
+            return { ...item, imageURL: null };
+          }
+        })
+      );
+
+      setCategories(categoriesWithImages);
+      setLoading(false); // End loading after data is set
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+      setLoading(false); // End loading in case of error
+    }
+  };
+
+  useEffect(() => {
+    handleGetCategories();
+  }, []);
+
+  useEffect(() => {
+    if (swiperRef.current && !loading) {
+      swiperRef.current.swiper.update();
+    }
+  }, [categories, loading]);
+
   return (
     <Swiper
-      slidesPerView={slides}
+      ref={swiperRef}
+      key={categories.length}
+      slidesPerView={4}
       spaceBetween={40}
       modules={[Autoplay, Navigation]}
       navigation={{
@@ -41,91 +99,32 @@ export const Category = ({ prevButton, nextButton, slides }) => {
       }}
       className="w-full"
     >
-      <SwiperSlide className="w-full">
-        <Link to={`/shop/product-category/chair`}>
-          <div className="w-full flex sm:flex-row flex-col items-center sm:justify-start justify-center gap-10">
-            <div className="w-36 h-36 flex items-center justify-center bg-[#e7e7e7] rounded-full">
-              <img
-                src={chairCategory}
-                className="w-[80%] h-[80%] object-contain"
-                alt="chair-category"
-              />
-            </div>
-            <div className="flex flex-col sm:text-start text-center gap-3">
-              <h2 className="text-4xl font-extrabold">Chair</h2>
-              <p className="text-3xl">7 Products</p>
-            </div>
-          </div>
-        </Link>
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <Link to={`/shop/product-category/lamp`}>
-          <div className="w-full flex sm:flex-row flex-col items-center sm:justify-start justify-center gap-10">
-            <div className="w-36 h-36 flex items-center justify-center bg-[#e7e7e7] rounded-full">
-              <img
-                src={lampCategory}
-                className="w-[80%] h-[80%] object-contain"
-                alt="lamp-category"
-              />
-            </div>
-            <div className="flex flex-col sm:text-start text-center gap-3">
-              <h2 className="text-4xl font-extrabold">Lamp</h2>
-              <p className="text-3xl">7 Products</p>
-            </div>
-          </div>
-        </Link>
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <Link to={`/shop/product-category/table`}>
-          <div className="w-full flex sm:flex-row flex-col items-center sm:justify-start justify-center gap-10">
-            <div className="w-36 h-36 flex items-center justify-center bg-[#e7e7e7] rounded-full">
-              <img
-                src={tableCategory}
-                className="w-[80%] h-[80%] object-contain"
-                alt="table-category"
-              />
-            </div>
-            <div className="flex flex-col sm:text-start text-center gap-3">
-              <h2 className="text-4xl font-extrabold">Table</h2>
-              <p className="text-3xl">7 Products</p>
-            </div>
-          </div>
-        </Link>
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <Link to={`/shop/product-category/bed`}>
-          <div className="w-full flex sm:flex-row flex-col items-center sm:justify-start justify-center gap-10">
-            <div className="w-36 h-36 flex items-center justify-center bg-[#e7e7e7] rounded-full">
-              <img
-                src={bedCategory}
-                className="w-[80%] h-[80%] object-contain"
-                alt="bed-category"
-              />
-            </div>
-            <div className="flex flex-col sm:text-start text-center gap-3">
-              <h2 className="text-4xl font-extrabold">Bed</h2>
-              <p className="text-3xl">7 Products</p>
-            </div>
-          </div>
-        </Link>
-      </SwiperSlide>
-      <SwiperSlide className="w-full">
-        <Link to={`/shop/product-category/sofa`}>
-          <div className="w-full flex sm:flex-row flex-col items-center sm:justify-start justify-center gap-10">
-            <div className="w-36 h-36 flex items-center justify-center bg-[#e7e7e7] rounded-full">
-              <img
-                src={sofaCategory}
-                className="w-[80%] h-[80%] object-contain"
-                alt="sofa-category"
-              />
-            </div>
-            <div className="flex flex-col sm:text-start text-center gap-3">
-              <h2 className="text-4xl font-extrabold">Sofa</h2>
-              <p className="text-3xl">7 Products</p>
-            </div>
-          </div>
-        </Link>
-      </SwiperSlide>
+      {loading ? (
+        <Loading />
+      ) : (
+        categories.length > 0 &&
+        categories.map((item) => {
+          return (
+            <SwiperSlide className="w-full" key={item._id}>
+              <Link to={`/shop/product-category/${item.name.toLowerCase()}`}>
+                <div className="w-full flex sm:flex-row flex-col items-center sm:justify-start justify-center gap-10">
+                  <div className="w-36 h-36 flex items-center justify-center bg-[#e7e7e7] rounded-full">
+                    <img
+                      src={item.imageURL}
+                      className="w-[80%] h-[80%] object-contain"
+                      alt="chair-category"
+                    />
+                  </div>
+                  <div className="flex flex-col sm:text-start text-center gap-3">
+                    <h2 className="text-4xl font-extrabold">{item.name}</h2>
+                    <p className="text-3xl">{item.productCount} Products</p>
+                  </div>
+                </div>
+              </Link>
+            </SwiperSlide>
+          );
+        })
+      )}
     </Swiper>
   );
 };
--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ export const Loading = () => {
   return (
     <div className="w-full h-full py-10 flex items-center justify-center">
       <div
-        className="inline-block h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1s_linear_infinite]"
+        className="inline-block h-16 w-16 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1s_linear_infinite]"
         role="status"
       ></div>
     </div>

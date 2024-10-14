82f2cdbbf21217ce4ca507e9e0311a62981c755d
Your Name <you@example.com>
2024-10-14 22:43:18 +0530

    Updated

--- admin/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const ProtectedRoute = ({ isLoggedIn, children }) => {
-  if (!isLoggedIn) {
-    return <Navigate to="/admin-signin" />;
-  }
-  return children;
-};
